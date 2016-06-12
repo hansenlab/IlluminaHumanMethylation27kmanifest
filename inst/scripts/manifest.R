53b789213a3dce5c808cbdee5ff4c15497abad6b
jfortin <jfortin@jhsph.edu>
2016-06-12 15:02:16 -0400

    Added colors

--- inst/scripts/manifest.R
+++ inst/scripts/manifest.R
@@ -1,76 +1,21 @@
 library(minfi)
 
-manifest <- "../../../files_27k/humanmethylation27_270596_v1-2.csv"
+manifest <- "../../../files_27k/HumanMethylation27_270596_v.1.2.bpm"
 stopifnot(file.exists(manifest))
-
-
-read.manifest.27k <- function(file) {
-	temp <- read.csv(file)
-	control.line <- which(temp[,1]=="[Controls]")
-	assay.line   <- which(temp[,1]=="[Assay]")
-	colNames <- temp[assay.line+1,]
-	colClasses <- rep("character", length(colNames))
-
-	manifest <- read.csv(file, header = TRUE,
-	                           sep = ",", comment.char = "", quote = "", 
-	                           skip = assay.line+1, colClasses = colClasses,
-	                           nrows = control.line - (assay.line + 1), fill = TRUE)
-
-	TypeI <- manifest[ c("Name", "AddressA_ID", "AddressB_ID", "Color_Channel",
-	                         "Next_Base", "AlleleA_ProbeSeq", "AlleleB_ProbeSeq")]
-	TypeI <- TypeI[TypeI$Name != "",]
-	names(TypeI)[c(2,3,4,5,6,7)] <- c("AddressA", "AddressB", "Color", "NextBase",
-	                                  "ProbeSeqA", "ProbeSeqB")
-	TypeI <- as(TypeI, "DataFrame")
-	TypeI$ProbeSeqA <- DNAStringSet(TypeI$ProbeSeqA)
-	TypeI$ProbeSeqB <- DNAStringSet(TypeI$ProbeSeqB)
-	TypeI$NextBase <- DNAStringSet(TypeI$NextBase)
-	TypeI$nCpG <- as.integer(oligonucleotideFrequency(TypeI$ProbeSeqB,
-	                                                  width = 2)[, "CG"] - 1)
-	TypeI$nCpG[TypeI$nCpG < 0] <- 0L
-
-	TypeII <- manifest[manifest$Infinium_Design_Type == "II",
-	                    c("Name", "AddressA_ID", "AlleleA_ProbeSeq")]
-	names(TypeII)[c(2,3)] <- c("AddressA", "ProbeSeqA")
-	TypeII <- as(TypeII, "DataFrame")
-	TypeII$ProbeSeqA <- BStringSet(TypeII$ProbeSeqA)
-	TypeII$nCpG <- as.integer(letterFrequency(TypeII$ProbeSeq, letters = "R"))
-
-	controls <- read.table(file, skip = control.line+1,
-	                           sep = ",", comment.char = "", quote = "",
-	                           colClasses = c(rep("character", 4)))
-
-	TypeControl <- controls[, 1:4]
-	names(TypeControl) <- c("Address", "Type", "Color", "ExtendedType")
-	TypeControl <- as(TypeControl, "DataFrame")
-
-	snps <- TypeControl[TypeControl$Type == "Genotyping",]
-	TypeControl <- TypeControl[TypeControl$Type != "Genotyping",]
-	rsname <- sub("_[AB]", "", snps$ExtendedType)
-	snps.sp <- split(snps, rsname)
-	snps.sp <- lapply(names(snps.sp), function(rs) {
-	    snp <- snps.sp[[rs]]
-	    DataFrame(Name = rs,
-	              AddressA = snp[grep("_A", snp$ExtendedType), "Address"],
-	              AddressB = snp[grep("_B", snp$ExtendedType), "Address"],
-	              Color = "Unknown")
-	})
-	TypeSnpI <- do.call(rbind, snps.sp)
-	TypeSnpII <- TypeSnpI[0,]
-	     
-
-	list(manifestList = list(TypeI = TypeI, TypeII = TypeII, TypeControl = TypeControl,
-	TypeSnpI = TypeSnpI, TypeSnpII = TypeSnpII),
-	manifest = manifest, controls = controls)  
-}
-
-
-
-
-maniTmp <- read.manifest.27k(manifest)
+maniTmp <- minfi:::read.manifest.27k(manifest)
 
 ## Manifest package
 maniList <- maniTmp$manifestList
+
+# Adding colors to Type I SNP probes as discovered experimentally by Tim Triche:
+maniList$TypeSnpI[match(
+        c("rs1019916", "rs10457834", "rs1416770", "rs1941955", "rs2125573", 
+        "rs2235751", "rs2521373", "rs264581", "rs2804694", "rs2959823", 
+        "rs5931272", "rs6546473", "rs739259", "rs798149", "rs845016", 
+        "rs866884"), maniList$TypeSnpI$Name), "Color"] <- 
+        c("Red", "Red", "Red", "Red", "Red", "Grn", "Grn", "Red", "Red", 
+		    "Red", "Red", "Grn", "Red", "Grn", "Grn", "Red")
+
 IlluminaHumanMethylation27kmanifest <- IlluminaMethylationManifest(TypeI = maniList$TypeI,
                                                                     TypeII = maniList$TypeII,
                                                                     TypeControl = maniList$TypeControl,
@@ -80,9 +25,3 @@ IlluminaHumanMethylation27kmanifest <- IlluminaMethylationManifest(TypeI = maniL
 stopifnot(validObject(IlluminaHumanMethylation27kmanifest))
 save(IlluminaHumanMethylation27kmanifest, compress = "xz",
      file = "../../data/IlluminaHumanMethylation27kmanifest.rda")
-
-
-
-
-
-
